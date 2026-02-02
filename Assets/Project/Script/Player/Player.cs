b9011e10190c9d97219bbbfbe3fd2c36aefc03ad
dbxbqmdyd <dbxbqmdyd019@gmail.com>
2026-02-02 18:43:24 +0900

    물리 변경
    이동을 트랜스가 아닌 리지드 물리로 변경 할 예정

--- Assets/Project/Script/Player/Player.cs
+++ Assets/Project/Script/Player/Player.cs
@@ -1,21 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 namespace Project.Script.Player
 {
+    public enum PlayerState
+    {
+        Idle,
+        Move,
+        Attack,
+        Dash,
+        Hit
+
+    }
     public class Player : MonoBehaviour
     {
-        // Start is called before the first frame update
+        Rigidbody2D rb;
+        PlayerState currentState;
+        Vector2 WASD()
+        {
+            float h = 0f;
+            float v = 0f;
+
+            if (Input.GetKey(KeyCode.A)) h -= 1f;
+            if (Input.GetKey(KeyCode.D)) h += 1f;
+            if (Input.GetKey(KeyCode.W)) v += 1f;
+            if (Input.GetKey(KeyCode.S)) v -= 1f;
+
+            return new Vector2(h, v).normalized;
+        }
         void Start()
         {
+            rb = GetComponent<Rigidbody2D>();
+            currentState = PlayerState.Idle;
+        }
+        public void ChangeState(PlayerState state)
+        {
+            if (currentState == state) return;
+            currentState = state;
+        }
+        public void Idle()
+        {
+            Vector2 input = WASD();
+            if (input.magnitude > 0.1f)
+            {
+                ChangeState(PlayerState.Move);
+            }
 
         }
-
-        // Update is called once per frame
-        void Update()
+        public void Move()
         {
+            Vector2 input = WASD();
+            if (input.magnitude > 0.1f)
+            {
+                transform.Translate(input * 5f * Time.deltaTime);
+            }
+            else
+            {
+                ChangeState(PlayerState.Idle);
+            }
+        }
+        void Update() // 입력만 여기로
+        {
+            switch (currentState)
+            {
+                case PlayerState.Idle:
+                    Idle();
+                    break;
+                case PlayerState.Move:
+                    Move();
+                    break;
+            }
 
+        }
+        private void FixedUpdate() // 리지드보디로 변경 트랜스폼 말고
+        {
+            switch (currentState)
+            {
+                case PlayerState.Idle:
+                    //Idle();
+                    break;
+                case PlayerState.Move:
+                    //Move();
+                    break;
+            }
         }
     }
 }
